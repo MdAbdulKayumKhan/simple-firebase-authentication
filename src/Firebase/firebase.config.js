cd42ac70b452f5e16c170b62ea32551600265bf6
MdAbdulKayumKhan <kayum261@gmail.com>
2021-10-07 17:30:00 +0600

    added second commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2m1zYvrA_rsDdubPpIQ3EYD_HQASdKrg",
+  authDomain: "simple-firebase-authenti-fc7a2.firebaseapp.com",
+  projectId: "simple-firebase-authenti-fc7a2",
+  storageBucket: "simple-firebase-authenti-fc7a2.appspot.com",
+  messagingSenderId: "888364292252",
+  appId: "1:888364292252:web:41f4a366e9b6eba63b7559"
+};
+
+export default firebaseConfig;
